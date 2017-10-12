88a669282e3b8ce1f045ae0417a2222f373229a7
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2017-10-12 11:49:15 +0200

    added initial implementations..

---
+++ ale-api/src/main/java/it/cnr/istc/ale/api/Event.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2017 Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package it.cnr.istc.ale.api;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import java.util.Collection;
+
+/**
+ *
+ * @author Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
+ */
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "type")
+@JsonSubTypes({
+    @Type(value = TextEvent.class, name = "text")
+    ,@Type(value = QuestionEvent.class, name = "question")})
+public abstract class Event {
+
+    private String name;
+    private String role;
+    private Collection<Condition> tr_conditions;
+    private Collection<Condition> ex_conditions;
+    private Collection<String> events;
+    private Collection<Relation> relations;
+}
