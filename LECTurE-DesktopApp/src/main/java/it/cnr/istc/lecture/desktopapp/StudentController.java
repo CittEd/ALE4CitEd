f3614d9e3161f5a02029697f3b2b0a0ea7b3b7e5
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2018-04-06 15:45:10 +0200

    added initial student graph..

--- LECTurE-DesktopApp/src/main/java/it/cnr/istc/lecture/desktopapp/StudentController.java
+++ LECTurE-DesktopApp/src/main/java/it/cnr/istc/lecture/desktopapp/StudentController.java
@@ -18,6 +18,12 @@ package it.cnr.istc.lecture.desktopapp;
 
 import it.cnr.istc.lecture.desktopapp.Context.ParameterValue;
 import java.net.URL;
+import java.text.FieldPosition;
+import java.text.NumberFormat;
+import java.text.ParsePosition;
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
@@ -29,6 +35,14 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.StackPane;
+import javafx.util.StringConverter;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.fx.ChartViewer;
+import org.jfree.chart.plot.CombinedDomainXYPlot;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 /**
  *
@@ -49,16 +63,21 @@ public class StudentController implements Initializable {
     @FXML
     private StackPane student_chart_pane;
     private final ObjectProperty<StudentContext> std_ctx = new SimpleObjectProperty<>();
+    private final Map<StudentContext, StudentChartContext> std_ctxs = new IdentityHashMap<>();
+    private static final StringConverter TIME_STRING_CONVERTER = new TimeStringConverter();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         name_column.setCellValueFactory(new PropertyValueFactory<>("name"));
         value_column.setCellValueFactory(new PropertyValueFactory<>("value"));
+
         std_ctx.addListener((ObservableValue<? extends StudentContext> observable, StudentContext oldValue, StudentContext newValue) -> {
+            student_chart_pane.getChildren().clear();
             if (newValue != null) {
                 first_name.setText(newValue.getStudent().first_name);
                 last_name.setText(newValue.getStudent().last_name);
                 parameters_table_view.setItems(newValue.parametersProperty());
+                student_chart_pane.getChildren().add(std_ctxs.computeIfAbsent(newValue, ctx -> new StudentChartContext(ctx)).viewer);
             } else {
                 first_name.setText(null);
                 last_name.setText(null);
@@ -69,5 +88,42 @@ public class StudentController implements Initializable {
 
     public ObjectProperty<StudentContext> studentContextProperty() {
         return std_ctx;
+    }
+
+    private class StudentChartContext {
+
+        private final StudentContext ctx;
+        private final Map<String, XYSeriesCollection> par_collections = new HashMap<>();
+        private final Map<String, Map<String, XYSeries>> par_series = new HashMap<>();
+        private final Map<String, XYPlot> par_plot = new HashMap<>();
+        private final ChartViewer viewer;
+
+        private StudentChartContext(StudentContext ctx) {
+            this.ctx = ctx;
+
+            final NumberAxis domain_axis = new NumberAxis("");
+            domain_axis.setNumberFormatOverride(new NumberFormat() {
+                @Override
+                public StringBuffer format(double number, StringBuffer toAppendTo, FieldPosition pos) {
+                    return format((long) number, toAppendTo, pos);
+                }
+
+                @Override
+                public StringBuffer format(long number, StringBuffer toAppendTo, FieldPosition pos) {
+                    return toAppendTo.append(TIME_STRING_CONVERTER.toString(number));
+                }
+
+                @Override
+                public Number parse(String source, ParsePosition parsePosition) {
+                    throw new UnsupportedOperationException("Not supported yet.");
+                }
+            });
+            CombinedDomainXYPlot plot = new CombinedDomainXYPlot(domain_axis);
+            plot.setGap(10.0);
+
+            JFreeChart chart = new JFreeChart(null, JFreeChart.DEFAULT_TITLE_FONT, plot, false);
+            chart.setBackgroundPaint(java.awt.Color.WHITE);
+            this.viewer = new ChartViewer(chart);
+        }
     }
 }
