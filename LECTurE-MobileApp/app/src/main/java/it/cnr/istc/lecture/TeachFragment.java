ab4f3ec59d91ec6419a92f11872aabc3f092786e
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2018-07-06 16:49:34 +0200

    added tab layout..

---
+++ LECTurE-MobileApp/app/src/main/java/it/cnr/istc/lecture/TeachFragment.java
@@ -0,0 +1,23 @@
+package it.cnr.istc.lecture;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TeachFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_teach, container, false);
+    }
+}
