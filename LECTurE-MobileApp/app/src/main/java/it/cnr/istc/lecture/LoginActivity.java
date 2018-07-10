de43e29b064446c56ac22e7c63f9dd4bc3c1cab2
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2018-07-10 18:11:52 +0200

    minor cleanings..

--- LECTurE-MobileApp/app/src/main/java/it/cnr/istc/lecture/LoginActivity.java
+++ LECTurE-MobileApp/app/src/main/java/it/cnr/istc/lecture/LoginActivity.java
@@ -3,7 +3,7 @@ package it.cnr.istc.lecture;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Login extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
